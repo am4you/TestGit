b6346d4fe5f297a9af87b229b0e5e9974525ba95
am4you <info@am4you.net>
2013-01-07 10:19:51 +0100

    Commit#1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestGit::Application.config.secret_token = '39542e608c9e8573c468204f88122ce3d832cd697f692fffd6c43c49f3733d4d96c7b73e23a8b00b2f6f2a2f041448e6ea4957fb5d70470933bb863f2a173bdd'
